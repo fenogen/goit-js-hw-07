53f1172cda042788d6838275f1d1ecf558475a7f
Oleg Soldatenko <sunskays@gmail.com>
2020-12-30 19:19:46 +0200

    Task #8(not ready)

--- js/task_2.js
+++ js/task_2.js
@@ -16,7 +16,7 @@ const list = document.querySelector('#ingredients')
 ingredients.forEach((i) => {                          //-----> Перебрали
   let item = document.createElement('li');            //-----> Создали элемент списка
   list.appendChild(item);                             //-----> Привязали к родителю
-  item.textContent = i;                               //-----> Дали название
+  item.textContent = i;                              //-----> Дали название
   return;
 });
 console.log(list.children);
